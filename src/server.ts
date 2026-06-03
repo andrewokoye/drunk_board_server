41e3fccb0357b5a92ae6753e749120a09c9db70c
psyao13 <psyao13@exmail.nottingham.ac.uk>
2026-06-03 02:32:01 +0100

    ANON > SRK

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
-import { Player, Room } from "./interfaces";
 import { supabase } from "./lib/supabase";
 
 const app = express();
